33638c2c3868f431d63964a035775c71ad316c75
gustavoscruz <gustadscruz@gmail.com>
2023-11-16 17:36:18 -0300

    modified AnimalAB Class

--- src/lista_04/AnimalAB.java
+++ src/lista_04/AnimalAB.java
@@ -5,6 +5,15 @@ abstract public class AnimalAB implements AnimalIF{
     private int quantidadePercorrida = 0;
     private int quantidadeDormida = 0;
 
+    private String nome;
+    private String tipo;
+    private int idade;
+    private String habitat;
+    private int quantidadeDePatas;
+
+    private double altura;
+    private double peso;
+
     public AnimalAB() {
     }
 
@@ -43,4 +52,60 @@ abstract public class AnimalAB implements AnimalIF{
     public void dormir(){
         quantidadeDormida++;
     };
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public int getQuantidadeDePatas() {
+        return quantidadeDePatas;
+    }
+
+    public void setQuantidadeDePatas(int quantidadeDePatas) {
+        this.quantidadeDePatas = quantidadeDePatas;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
 }
