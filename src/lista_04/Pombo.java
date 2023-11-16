3b2b6a381597b9cd585c834a85ef9f49eefe2b6c
gustavoscruz <gustadscruz@gmail.com>
2023-11-16 17:26:38 -0300

    modified Pombo Class

--- src/lista_04/Pombo.java
+++ src/lista_04/Pombo.java
@@ -1,4 +1,20 @@
 package lista_04;
 
-public class Pombo {
+public class Pombo extends AnimalVoadorAB{
+    public Pombo(){}
+    @Override
+    public void comer(){
+        setQuantidadeDeComida(1);
+    };
+    @Override
+    public void voar(){
+        setQuantidadePercorrida(500);
+    };
+
+    @Override
+    public void dormir(){
+        setQuantidadeDormida(8);
+    };
+
+
 }
