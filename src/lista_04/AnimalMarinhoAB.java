d86fd4c9a9cb8d8550ab997a2d1c3d5c1e3cc6a8
gustavoscruz <gustadscruz@gmail.com>
2023-11-16 17:36:30 -0300

    modified AnimalMarinhoAB Class

--- src/lista_04/AnimalMarinhoAB.java
+++ src/lista_04/AnimalMarinhoAB.java
@@ -1,7 +1,9 @@
 package lista_04;
 
 abstract public class AnimalMarinhoAB extends AnimalAB{
-    int nadar = 0;
+    private int nadar = 0;
+    private int quantidadeDeAsas;
+    private float envergaduraDaAsa;
     public AnimalMarinhoAB(){}
 
     public int getNadar() {
@@ -10,6 +12,22 @@ abstract public class AnimalMarinhoAB extends AnimalAB{
 
     public void setNadar(int nadar) {
         this.nadar = nadar;
+    }
+
+    public int getQuantidadeDeAsas() {
+        return quantidadeDeAsas;
+    }
+
+    public void setQuantidadeDeAsas(int quantidadeDeAsas) {
+        this.quantidadeDeAsas = quantidadeDeAsas;
+    }
+
+    public float getEnvergaduraDaAsa() {
+        return envergaduraDaAsa;
+    }
+
+    public void setEnvergaduraDaAsa(float envergaduraDaAsa) {
+        this.envergaduraDaAsa = envergaduraDaAsa;
     }
 
     public void nadar(){
