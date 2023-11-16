adf3acd71a1b662a79e50307d31de164d6e514e6
gustavoscruz <gustadscruz@gmail.com>
2023-11-16 16:34:00 -0300

    modified AnimalVoadorAB

--- src/lista_04/AnimalVoadorAB.java
+++ src/lista_04/AnimalVoadorAB.java
@@ -1,4 +1,18 @@
 package lista_04;
 
 abstract public class AnimalVoadorAB extends AnimalAB {
+    int voo = 0;
+    public AnimalVoadorAB(){}
+
+    public int getVoo() {
+        return voo;
+    }
+
+    public void setVoo(int voo) {
+        this.voo = voo;
+    }
+
+    public void voar(){
+        voo++;
+    }
 }
