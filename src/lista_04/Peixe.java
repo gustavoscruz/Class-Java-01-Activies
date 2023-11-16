fca5cb520719b3a9b6b569540ecdf6be13664b25
gustavoscruz <gustadscruz@gmail.com>
2023-11-16 17:31:00 -0300

    modified Peixe Class

--- src/lista_04/Peixe.java
+++ src/lista_04/Peixe.java
@@ -1,4 +1,18 @@
 package lista_04;
 
-public class Peixe {
+public class Peixe extends AnimalMarinhoAB{
+    public Peixe(){}
+    @Override
+    public void comer(){
+        setQuantidadeDeComida(1);
+    };
+    @Override
+    public void nadar(){
+        setQuantidadePercorrida(25);
+    };
+
+    @Override
+    public void dormir(){
+        setQuantidadeDormida(8);
+    };
 }
